b53dbab92b9aed0294a898f29ec83257acc743f4
Madeline Stokes <115516126+sunshinemadeline@users.noreply.github.com>
2026-04-16 14:50:20 -0700

    perlin noise assignment 1

--- Assets/Scripts/Minecraft_WorldGenerator.cs
+++ Assets/Scripts/Minecraft_WorldGenerator.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class Minecraft_WorldGenerator : MonoBehaviour
 {
-    public int width = 25;
-    public int length = 25;
-    public int height = 3;
+    public int width = 20;
+    public int length = 20;
+
+    public int worldMinY = 0;
+    public int baseHeight = 4;
+    public int noiseHeight = 6;
+    public int seed = 12345;
+    public float noiseScale = 0.1f;
 
     public Minecraft_CubeSpawner cubeSpawner;
+
+    private float seedOffsetX;
+    private float seedOffsetZ;
 
     void Awake()
     {
@@ -14,6 +22,10 @@ public class Minecraft_WorldGenerator : MonoBehaviour
         {
             cubeSpawner = FindFirstObjectByType<Minecraft_CubeSpawner>();
         }
+
+        Random.InitState(seed);
+        seedOffsetX = Random.Range(0f, 9999f);
+        seedOffsetZ = Random.Range(0f, 9999f);
     }
 
     void Start()
@@ -25,29 +37,38 @@ public class Minecraft_WorldGenerator : MonoBehaviour
     {
         for (int x = 0; x < width; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int z = 0; z < length; z++)
             {
-                for (int z = 0; z < length; z++)
+                int terrainHeight = GetTerrainHeight(x, z);
+
+                for (int y = worldMinY; y <= terrainHeight; y++)
                 {
-                    CubeType type = ChooseBlockType(x, y, z);
+                    CubeType type = ChooseBlockType(y, terrainHeight);
                     cubeSpawner.SpawnCube(new Vector3(x, y, z), type);
                 }
             }
         }
     }
 
-    CubeType ChooseBlockType(int x, int y, int z)
+    int GetTerrainHeight(int x, int z)
     {
-        if (y == height - 1)
-        {
+        float sampleX = seedOffsetX + x * noiseScale;
+        float sampleZ = seedOffsetZ + z * noiseScale;
+
+        float noiseValue = Mathf.PerlinNoise(sampleX, sampleZ);
+
+        int heightOffset = Mathf.RoundToInt(noiseValue * noiseHeight);
+        return baseHeight + heightOffset;
+    }
+
+    CubeType ChooseBlockType(int y, int terrainHeight)
+    {
+        if (y == terrainHeight)
             return CubeType.Grass;
-        }
 
-        if (y == 0)
-        {
-            return CubeType.Stone;
-        }
+        if (y >= terrainHeight - 2)
+            return CubeType.Dirt;
 
-        return CubeType.Dirt;
+        return CubeType.Stone;
     }
 }
